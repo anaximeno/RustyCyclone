ed56e46d1a5877a8988b5322704a1e6eaba72b0a
anaximeno <anaximenobrito@gmail.com>
2022-03-26 14:46:21 -0100

    small fomating updates

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,10 +9,8 @@ fn main() {
     v.invert();
     v.inplace_mult(2.0);
 
-    let j = Vector3::from(1.3, 0.5, 1.2);
-
+    let j: Vector3 = Vector3::from(1.3, 0.5, 1.2);
     let k: Real = v.dot_product(&j);
-
     let h: Vector3 = v.vector_product(&j);
 
     println!("Dot Prod is = {:#?}\n", k);
