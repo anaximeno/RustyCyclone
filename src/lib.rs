0f4f7c4e79874d405a9f9c47bd68aee274db3f94
anaximeno <anaximenobrito@gmail.com>
2022-03-26 00:51:29 -0100

    added some basic vector operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,25 @@
-#!allow(dead_code)
-
 #[cfg(test)]
 mod tests {
+    use super::core::*;
+    use super::precision::*;
+
     #[test]
-    fn it_works() {
-        use super::precision::Real;
-        use super::core::Vector3;
-
+    fn vec3_magnitude() {
         let mut vec = Vector3::from(2., 1., -3.);
-
         vec.invert();
+        assert_eq!((14.0 as Real).sqrt(), vec.magnitude());
+    }
 
-        let magnitude: Real = 14_f32.sqrt();
-
-        assert_eq!(magnitude, vec.magnitude());
+    #[test]
+    fn vec3_inplace_mult() {
+        let mut vec = Vector3::from(1., 2., 1.);
+        vec.inplace_mult(2.0);
+        assert_eq!(4.0 as Real, vec.y);
     }
 }
 
 pub mod precision {
+    /// Defines a real number precision.
     pub type Real = f32;
 }
 
@@ -33,6 +35,7 @@ pub mod core {
     }
 
     impl Vector3 {
+        /// Returns a new Vector with all elements set to zero.
         pub fn new() -> Self {
             Vector3 {
                 x: 0.,
@@ -42,6 +45,18 @@ pub mod core {
             }
         }
 
+        /// Returns a Vector with the elements as the arguments
+        /// 
+        /// # Arguments
+        /// * `x` - value for the x axes
+        /// * `y` - value for the y axes
+        /// * `z` - value for the z axes
+        /// 
+        /// # Examples
+        /// ```
+        /// use rusty_cyclone::core::Vector3;
+        /// let vec = Vector3::from(1.2, 3.0, 1.0);
+        /// ```
         pub fn from(x: Real, y: Real, z: Real) -> Self {
             Vector3 {x, y, z, pad: 0.}
         }
@@ -60,18 +75,60 @@ pub mod core {
             self.x.powi(2) + self.y.powi(2) + self.z.powi(2)
         } 
 
-        pub fn multiply_inplace(&mut self, value: Real) {
+        pub fn normalize(&mut self) {
+            let m = self.magnitude();
+            
+            if m > 0.0 {
+                self.inplace_mult(1.0 / m);
+            }
+        }
+
+        pub fn inplace_mult(&mut self, value: Real) {
             self.x *= value;
             self.y *= value;
             self.z *= value;
         }
 
-        pub fn normalize(&mut self) {
-            let m = self.magnitude();
+        pub fn mult(self, value: Real) -> Vector3 {
+            Vector3::from(
+                self.x * value,
+                self.y * value,
+                self.z * value,
+            )
+        }
 
-            if m > 0_f32 {
-                self.multiply_inplace(1_f32 / m);
-            }
+        pub fn inplace_add(&mut self, v: Vector3) {
+            self.x += v.x;
+            self.y += v.y;
+            self.z += v.z;
+        }
+
+        pub fn add(&mut self, v: Vector3) -> Vector3 {
+            Vector3::from(
+                self.x + v.x,
+                self.y + v.y,
+                self.z + v.z,
+            )
+        }
+
+        pub fn inplace_sub(&mut self, v: Vector3) {
+            self.x -= v.x;
+            self.y -= v.y;
+            self.z -= v.z;
+        }
+
+        pub fn sub(&mut self, v: Vector3) -> Vector3 {
+            Vector3::from(
+                self.x - v.x,
+                self.y - v.y,
+                self.z - v.z
+            )
+        }
+
+        pub fn inplace_add_scaled_vector(&mut self, v: Vector3, scale: Real) {
+            self.x += v.x * scale;
+            self.y += v.y * scale;
+            self.z += v.z * scale;
         }
     }
 }
